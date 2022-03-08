8c12cb5d98c3fba21d60d28e3b12c5ab6ef66908
Marlin (DLR) <marlinmueller.mm@gmail.com>
2022-03-08 11:49:11 +0100

    optimize to only require POI and buffer size (POI_radius)

--- GeoJSON2CSV_GNSS_Points/litchi_mission_creator.py
+++ GeoJSON2CSV_GNSS_Points/litchi_mission_creator.py
@@ -1,22 +1,24 @@
-import csv
 import os
+from functools import partial
+import pyproj
+from shapely.ops import transform
 import fiona
 import pandas as pd
 import geopandas as gpd
 import numpy as np
 
-# Set path to waypoint (line) shapefile
-waypoint_shapefile = "C:/Users/muel_m31/Desktop/UCEA/Litchi/Shapes/litchi_test_file1.shp"
+
 # Set path to POI (point) shapefile
 poi_shapefile = "C:/Users/muel_m31/Desktop/UCEA/Litchi/Shapes/litchi_test_file_centerpoint.shp"
 
 # Set output path and output filename:
 out_path = "C:/Users/muel_m31/Desktop/UCEA/Litchi/Missions"
-out_filename = "jena_forst_test.csv"
+out_filename = "jena_forst_test3.csv"
 
 # Set other flight parameters:
-altitude = 30  # meters
+altitude = 120  # meters
 photo_timeinterval = 3  # seconds
+poi_radius = 150  # meters
 
 # Load default Litchi mission file
 litchi_default_mission = "empty_litchi_mission.csv"
@@ -40,16 +42,36 @@ def extract_points_from_shape(path_to_file):
             return point_coordinates
 
 
-# Get coordinates to variable
-shapefile_coordinates = extract_points_from_shape(waypoint_shapefile)
-poi_coordinates = extract_points_from_shape(poi_shapefile)
+from shapely.geometry import Point, mapping
 
-# Initialize
-# output_string = ""
+# Get coordinates to variable
+poi_coordinates = extract_points_from_shape(poi_shapefile)[0:2]
+poi_geoseries = gpd.GeoSeries([Point(poi_coordinates)])
+poi_coord = mapping(poi_geoseries)["features"][0]["geometry"]["coordinates"]
+
+# Set initial projection
+proj_wgs84 = pyproj.Proj('+proj=longlat +datum=WGS84')
+
+
+# Convert coordinates to Azimuthal equidistant projection to generate round buffer around POI
+def geodesic_point_buffer(lat, lon, m):
+    # Thanks to: https://gis.stackexchange.com/a/289923
+    # Azimuthal equidistant projection
+    aeqd_proj = '+proj=aeqd +lat_0={lat} +lon_0={lon} +x_0=0 +y_0=0'
+    project = partial(
+        pyproj.transform,
+        pyproj.Proj(aeqd_proj.format(lat=lat, lon=lon)),
+        proj_wgs84)
+    # Create buffer
+    buf = Point(0, 0).buffer(m)  # distance in metres
+    return transform(project, buf).exterior.coords[:]
+
+
+# Call function to generate round buffer with WGS84 coordinates
+buffer_coordinates = geodesic_point_buffer(poi_coord[1], poi_coord[0], poi_radius)
 
 # Initialize Litchi default dictionary
 litchi_dict = dict(zip(column_titles_list, default_mission_values))
-
 mission_values_array = []
 
 # Get datatpye for each parameter and set correct one for POI parameters
@@ -58,7 +80,8 @@ dataTypeDict["poi_latitude"] = np.dtype(float)
 dataTypeDict["poi_longitude"] = np.dtype(float)
 
 # Append coordinate of each waypoint to the output dataset
-for coordinate in shapefile_coordinates:
+# for coordinate in shapefile_coordinates:
+for coordinate in buffer_coordinates:
     # print(coordinate)
     # output_string = column_titles + litchi_dict["latitude"]
 
